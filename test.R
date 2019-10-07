f5af3cb5b86430c8b93579c25bf7c41ce6de6ce9
Grimes <ngrim732@uwsp.edu>
2019-10-07 17:39:36 -0500

    Histogram commit

---
+++ test.R
@@ -0,0 +1,7 @@
+WVSData<-read.csv(file="C:\\Users\\ngrim732\\Documents\\WV_US.csv",header=TRUE,sep=",")
+summary(WVSData$v23)
+str(WVSData)
+myReg=lm(V23~V59,WVSData)
+summary(myReg)
+summary(WVSData$V23)
+hist(WVSData$V23)
